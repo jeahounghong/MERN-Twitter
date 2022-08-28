a279707e592e52bc355fe3862dfae390f3a10e26
“Jeahoung <jeahounghong@gmail.com>
2022-08-28 02:18:16 -0400

    userAuth

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+// IMPORTS
 const express = require("express");
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const passport = require('passport');
+
 const app = express();
 
-app.get("/", (req,res) => res.send("Hello Korld"));
+// MIDDLEWARE
+app.use(passport.initialize());
+require('./config/passport')(passport);
+// app.get("/", (req,res) => res.send("Hello Korld"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
@@ -15,3 +23,10 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
