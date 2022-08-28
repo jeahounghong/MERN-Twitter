1e85856bfc694cb29a207b4aef8610445144fbe5
“Jeahoung <jeahounghong@gmail.com>
2022-08-28 10:12:24 -0400

    loading frontend work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Root from './components/root';
+import configureStore from './store/store';
+import jwt_decode from 'jwt-decode';
+import { setAuthToken } from './util/session_api_util';
+import { logout } from './actions/session_actions';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+    store = configureStore(preloadedState);
+    const currentTime = Date.now() / 1000;
+    if (decodedUser.exp < currentTime) {
+      // Logout the user and redirect to the login page
+      store.dispatch(logout());
+      window.location.href = '/login';
+    }
+  } else {
+    store = configureStore({});
+  }
+
+  const root = document.getElementById('root');
+
+  // debugger;
+  ReactDOM.render(<Root store={store} />, root);
+
+});
+
+
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
